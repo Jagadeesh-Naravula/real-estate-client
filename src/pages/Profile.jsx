72429e8bb00cb2482cfcbe06cd54b219ab5e1c8f
Jagadeesh N <n.jagadeesh@simplyfi.tech>
2024-03-23 09:11:33 +0530

    image upload to functionality

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,12 +1,52 @@
+import { useEffect, useRef, useState } from 'react';
 import { useSelector } from 'react-redux';
+import { getDownloadURL, getStorage, ref, uploadBytesResumable } from 'firebase/storage';
+import {app} from '../firebase.js'
 
 const Profile = () => {
   const { currentUser } = useSelector((state)=> state.user);
+  const fileRef = useRef(null);
+  const [file, setFile] = useState(undefined);
+  const [imgPerc, setImgPerc] = useState(0);
+  const [fileUploadError, setFileUploadError] = useState(false);
+  const [formData, setFormData] = useState({});
+
+  console.log(formData)
+
+  useEffect(()=>{
+    if(file){
+      handleFileUpload(file);
+    }
+  },[file]);
+
+  const handleFileUpload = (file)=>{
+    const storage = getStorage(app);
+    const fileName = new Date().getTime() + file.name;
+    const storageRef = ref(storage, fileName);
+    const uploadTask = uploadBytesResumable(storageRef, file);
+
+    uploadTask.on('state_changed', (snapshot)=>{
+      const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+      console.log(progress)
+      setImgPerc(Math.round(progress));
+    },
+    (error)=>{
+      setFileUploadError(true);
+    },
+    ()=>{
+      getDownloadURL(uploadTask.snapshot.ref).then((downloadURL)=>{
+        setFormData({ ...formData, avatar: downloadURL})
+      })
+    })
+  }
+
   return (
     <div className='p-3 max-w-lg mx-auto'>
      <h1 className='font-semibold my-7 text-center text-3xl'> Profile </h1>
      <form className='flex flex-col gap-4'>
-      <img className='rounded-full h-24 w-24 object-cover self-center mt-2' src={currentUser.avatar} alt='User Image' />
+      <input onChange={(e)=> setFile(e.target.files[0])} type="file" ref={fileRef} hidden accept='image/*'/>
+      <img onClick={()=> fileRef.current.click()} className='rounded-full h-24 w-24 object-cover self-center mt-2' src={formData.avatar || currentUser.avatar} alt='User Image' />
+      <p className='text-sm self-center'>{fileUploadError? (<span className='text-red-700'>Error Image upload</span>) : (imgPerc > 0 && imgPerc < 100) ? (<span className='text-slate-700'>{`Uploading ${imgPerc}%`}</span>): imgPerc === 100 ? <span className='text-green-700'>Image Uploaded Successfully!</span> : "" }</p>
       <input type="text" placeholder='username' id='username' className='border p-3 rounded-lg' />
       <input type="text" placeholder='email' id='email' className='border p-3 rounded-lg' />
       <input type="text" placeholder='password' id='password' className='border p-3 rounded-lg' />
