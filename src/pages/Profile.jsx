e48f58989a60bdf53efce892a4afd69ad28318ea
Jagadeesh N <n.jagadeesh@simplyfi.tech>
2024-04-08 10:51:44 +0530

    listing delete api implementation

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -140,6 +140,23 @@ const Profile = () => {
       setShowListingsError(true);
     }
   }
+  
+  const handleListingDelete = async (listingId) =>{
+    try {
+      const res = fetch(`/api/listing/delete/${listingId}`, {
+        method: 'DELETE'
+      });
+      const data = (await res).json();
+      if(data.success === false){
+        console.log(data.message)
+        return;
+      }
+
+      setUserListings((prev) => prev.filter((listing)=> listing._id !== listingId));
+    } catch (error) {
+      console.log(error.message)
+    }
+  }
 
   return (
     <div className='p-3 max-w-lg mx-auto'>
@@ -176,7 +193,7 @@ const Profile = () => {
           </Link>
   
           <div className='flex flex-col items-center'>
-            <button className='text-red-700 uppercase'>Delete</button>
+            <button onClick={()=>handleListingDelete(listing._id)} className='text-red-700 uppercase'>Delete</button>
             <button className='text-green-700 uppercase'>Edit</button>
           </div>
         </div>
