d2edb739dfca6067219f620c8725341c2cdd97fa
Jagadeesh N <n.jagadeesh@simplyfi.tech>
2024-05-09 14:39:47 +0530

    show more button

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -96,6 +96,20 @@ export const Search = () => {
         navigate(`/search?${searchQuery}`);
     }
 
+    const onShowMoreClick =async () =>{
+        const numberOfListings = listings.length;
+        const startIndex = numberOfListings;
+        const urlParams = new URLSearchParams(location.search);
+        urlParams.set('startIndex', startIndex);
+        const searchQuery = urlParams.toString();
+        const res = await fetch(`/api/listing/get?${searchQuery}`);
+        const data = await res.json();
+        if(data.length < 8){
+            setShowMore(false);
+        }
+        setListings([...listings, ...data]);
+    }
+
   return (
     <div className='flex flex-col md:flex-row'>
         <div className='p-7 border-b-2 md:border-r-2 md:min-h-screen'>
@@ -167,6 +181,13 @@ export const Search = () => {
                     !loading && listings.length>0 && listings.map((listing)=>(
                         <ListingItem key={listing._id} listing={listing}/>
                     ))
+                }
+                {
+                    showMore && (
+                        <button className='text-green-700 hover:underline p-7 text-center w-full' onClick={onShowMoreClick}>
+                            Show more
+                        </button>
+                    )
                 }
             </div>
         </div>
