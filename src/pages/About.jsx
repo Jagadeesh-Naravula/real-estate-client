e09bc69d1ad25c1240747e6f75887c24486ba772
Jagadeesh N <n.jagadeesh@simplyfi.tech>
2024-05-10 10:53:56 +0530

    complete about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,7 +2,20 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>About</div>
+    <div className='mx-auto px-4 py-20 max-w-6xl'>
+      <h1 className='text-3xl font-bold mb-4 text-slate-800'>
+        About MyEstate
+      </h1>
+      <p className='mb-4 text-slate-700'>
+      Welcome to MyEstate, your gateway to exclusive real estate opportunities! Whether you're in search of your dream home or seeking a lucrative investment property, our platform offers a diverse range of listings to suit every need and preference. From sleek urban apartments to sprawling countryside estates, we connect you with the finest properties on the market
+      </p>
+      <p className='mb-4 text-slate-700'>
+      Discover unparalleled convenience as you browse through our user-friendly interface, complete with detailed descriptions, high-quality images, and interactive maps. With advanced search filters, finding the perfect property has never been easier. 
+      </p>
+      <p className='mb-4 text-slate-700'>
+      Our team of seasoned real estate experts is committed to providing personalized assistance every step of the way, ensuring a seamless experience from initial search to closing the deal. Whether you're buying, selling, or renting, trust Prime Property Portal to turn your real estate aspirations into reality. Start your journey today and unlock the door to your next chapter in luxury living. className='mb-4'
+      </p>
+    </div>
   )
 }
 
